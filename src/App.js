6e2125d07c5d6f57060e2f72e9eefd2a4d7b7063
SumanthBHegde <sumanthbhegde12@gmail.com>
2024-03-07 23:45:44 +0530

    ...

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
-import "./App.css";
+// Import necessary dependencies
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import axios from "axios";
 
-// Pages
+// Import your pages
 import HomePage from "./pages/Home.page";
 import MoviePage from "./pages/Movie.page";
 import PlayPage from "./pages/Play.page";
 
-// Routes
-import {Routes, Route} from 'react-router-dom';
-import axios from "axios";
-
-// React slick css
+// Import React Slick CSS
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-
-axios.defaults.baseURL= "https://api.themoviedb.org/3";
-axios.defaults.params ={};
-axios.defaults.params["api_key"]= process.env.REACT_APP_API_KEY;
+// Set up Axios defaults
+axios.defaults.baseURL = "https://api.themoviedb.org/3";
+axios.defaults.params = {};
+axios.defaults.params["api_key"] = process.env.REACT_APP_API_KEY;
 
 function App() {
-  return <Routes>
-    <Route path="/" element={<HomePage />} />
-    <Route path="/movie/:id" element={<MoviePage />} />
-    <Route path="/plays" element={<PlayPage />} />
-  </Routes>
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/movie/:id" element={<MoviePage />} />
+      <Route path="/plays" element={<PlayPage />} />
+    </Routes>
+  );
 }
 
 export default App;
